aa0eef901b0ee9ef005f71e7e6fbef6a673b45cb
laochen <937459807@qq.com>
2019-04-17 20:40:06 +0800

    Signed-off-by: laochen <937459807@qq.com>

---
+++ data/posts-data.js
@@ -0,0 +1,120 @@
+var local_database = [{
+    date: "Sep 18 2016",
+    title: "EDCON 2019",
+    imgSrc: "/images/post/01.jpg",
+    avatar: "/images/avatar/1.png",
+    content: "EDCON 2019 (Community Ethereum Development Conference) is a non-profit global conference which will happen in Sydney, Australia on 8-14 April 2019.",
+    reading: "112",
+    collection: "96",
+    headImgSrc: "/images/post/01.jpg",
+    author: "小青",
+    dateTime: "24小时前",
+    detail: "EDCON 2019 (Community Ethereum Development Conference) is a non-profit global conference which will happen in Sydney, Australia on 8-14 April 2019. The purpose of the conference is to improve communication among various Ethereum communities and to promote Ethereum ecosystem development on a global scale. EDCON is organized as a conference series, once a year in different countries. The previous EDCONs were held successfully in ESCP Europe, Paris 2017, and Ryerson University, Toronto 2018 (both were organized by LinkTime, one of the Chinese Ethereum community representatives). The Ethereum Foundation and various Ethereum communities have been strongly supportive to this high-quality community conference.",
+    postId: 0,
+    music: {
+      url: "http://dl.stream.qqmusic.qq.com/M500000T5NNf3Ez4ES.mp3?vkey=7725E5A7FE9C28628394FC3E4803390B531DD3BEA07CCF6F2141A9E0E1B006CFDE396E9A1C43973730DF57C0DA081657B7EE717B1C2CF054&guid=5150825362&fromtag=1",
+      title: "Prayer x - King Gnu",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R300x300M000002t9LLa3ONO4g.jpg"
+    }
+  },
+  {
+    title: "向日葵为什么向阳开",
+    content: "向日葵是一种常见的植物，它的花朵很漂亮。向日葵还有一个特点，那就是它的花总是向着太阳开的。",
+    imgSrc: "/images/post/02.jpg",
+    reading: 62,
+    detail: "向日葵是一种常见的植物，它的花朵很漂亮。向日葵还有一个特点，那就是它的花总是向着太阳开的。这是为什么呢？科学家们经过研究发现，这是植物生长素在起作用。植物生长素十分有趣，阳光照到哪里，它就从哪里溜掉，好像有意与太阳捉迷藏似的。早晨，向日葵的花盘朝东，生长素就从向阳的一面溜到背阳的一面，使那里的组织加快生长。结果，花盘的茎部背阳的部分长得快，拉长了；向阳的一面长得慢，于是植株就弯曲起来。向日葵的花盘就这样朝着太阳打转。那么，向日葵为什么向阳开呢？在葵花的大花盘四周，有一圈金黄色的舌状小花，中间是管状小花。管状小花中含的纤维很丰富，受到阳光照射后，温度升高了，基部的纤维会发生收缩。这一收缩，就使花盘主动地转换方向来接受阳光。可见，向日葵花盘的转动并不是由于光线的直接影响，而是受温度影响的缘故。",
+    collection: 92,
+    dateTime: "24小时前",
+    headImgSrc: "/images/post/02.jpg",
+    author: "迷的城",
+    date: "Nov 20 2016",
+    avatar: "/images/avatar/1.png",
+    postId: 1,
+    music: {
+      url: "http://other.web.nc01.sycdn.kuwo.cn/resource/n2/17/41/650858910.mp3",
+      title: "未知 - stay with me",
+      coverImg: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1551515348&di=0fc6a3b968cebb2f5501ef23b705cb64&src=http://img.cndesign.com/Works/20151217/dae0fa9947b34343b68cdabac47c997c.jpeg"
+    }
+  },
+  {
+    //按住alt + shift + F 可以格式化代码样式
+    title: "中国开发者真实现状：算法工程师最稀缺！",
+    content: "互联网的 2018 年，注定是不平凡的一年...",
+    detail: "浩浩荡荡的美国制裁中兴事件唤醒了科技界对芯片产业的重视，倒逼了一系列芯片方面的布局和投资；互联网人口红利不断消耗，推动百度、腾讯、阿里巴巴等科技巨头先后实施战略升级和组织架构调整；加密货币的严格审查直接导致了曾经辉煌无量的比特币暴跌，但数字货币的底层技术区块链却逐步生根落地，推动了下一代互联网建设；华为孟晚舟事件揭开了蛰伏已久的 5G 技术，而围绕 5G 科技主导权的争夺还将继续白热化......混沌之下，身处其中的开发者更是切身感受到了波涛澎湃下的技术趋势更迭。为此，CSDN 历时 143 天，重磅发布了 2018 - 2019 中国开发者调查报告：想了解最全面最写实的开发者画像吗？想了解哪些开发岗位最受推崇、薪资待遇最佳吗？想了解大数据、云计算、区块链、人工智能、物联网等技术领域的最新热点动态吗？......尽在此处！",
+    imgSrc: "/images/post/03.jpg",
+    headImgSrc: "/images/post/03.jpg",
+    reading: 62,
+    collection: 92,
+    author: "豆瓣",
+    date: "Nov 12 2016",
+    dateTime: "三天前",
+    avatar: "/images/avatar/3.png",
+    postId: 2,
+    music: {
+      url: "http://other.web.nc01.sycdn.kuwo.cn/resource/n2/17/41/650858910.mp3",
+      title: "未知 - stay with me",
+      coverImg: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1551515348&di=0fc6a3b968cebb2f5501ef23b705cb64&src=http://img.cndesign.com/Works/20151217/dae0fa9947b34343b68cdabac47c997c.jpeg"
+    }
+  },
+  {
+    title: "终于明白——区块链是个什么鬼",
+    content: "只让人们在比特币带来的交易中获取比特币，或许是有意义的。一旦有足够的人这样想，它就会变成一个自我实现的预言。”——中本聪，2009",
+    imgSrc: "/images/post/04.jpg",
+    reading: 102,
+    detail: "区块链本身是一个技术，这个技术本身是没有问题，也能解决很多问题，应用场景很丰富很广泛。但现在感觉区块链存在过度炒作的情况，技术本身没有坏处，只是不要借技术名义和噱头来进行炒作，而应该根据具体应用场景结合来推广区块链。一旦每个人都用尽一页纸的空间、无法记录进一步的交易，他们就开始卖力地计算该页纸的密封数字，使得它可以被藏入文件夹中。在网络中，每个人都进行这个计算，而最早算出密封数字的那个人会向其他所有人宣布这个数字。可是，如果他们知道其他人会算出它然后宣布它，为什么每个人都要耗费资源来进行计算呢？为什么不坐视不理、等着抱大腿呢？好问题。这就是激励加入到这幅图景的地方。每一个区块链的成员都有资格获得奖励。第一个计算出密封数字的人将得到免费的金钱作为对他的努力（比如：付出的 CPU 算力和电力）的奖励。",
+    collection: 92,
+    dateTime: "24小时前",
+    headImgSrc: "/images/post/04.jpg",
+    author: "小白",
+    date: "Nov 20 2016",
+    avatar: "/images/avatar/5.png",
+    postId: 3,
+    music: {
+      url: "http://other.web.nc01.sycdn.kuwo.cn/resource/n2/17/41/650858910.mp3",
+      title: "未知 - stay with me",
+      coverImg: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1551515348&di=0fc6a3b968cebb2f5501ef23b705cb64&src=http://img.cndesign.com/Works/20151217/dae0fa9947b34343b68cdabac47c997c.jpeg",
+    }
+  },
+  {
+    title: "5年内改变世界！VR技术的威力有多大？",
+    content: "VR内容场景的呈现分为两种情况：实景拍摄与3D建模场景制作。其中，3d建模场景制作又包含了“可以在VR里行走”和“不能在VR里行走”两种情况。",
+    imgSrc: "/images/post/05.jpg",
+    reading: 102,
+    detail: "上述简单说明了一个VR视频是如何制作的，但是一个VR视频如果不使用特殊的播放器，那么播放出来的效果看上去是一个拉伸的长条形图片拼成的视频。之所以Vr视频能让人有身处其中的感觉，是因为观看时人的视角处于VR视频空间中，而不是看到一个二维平面，因此就需要将本身的全景二维图，投影到一个立体的空间——立方体or球形。简单球面纹理映射讲解实现球面纹理映射有两种方法，一种是使用顶点的法向量来生成纹理坐标，另一个是使用顶点的位置向量来生成纹理坐标。想象一个球体，球面上每一个点的法向量，可以是从球心起到该点的线段距离。球上每一点都可以视作一个无限小的平面。Ps: 法向量——垂直于平面的向量，叫做该平面的法向量",
+    collection: 26,
+    dateTime: "24小时前",
+    headImgSrc: "/images/post/05.jpg",
+    author: "许宣",
+    date: "Nov 20 2016",
+    avatar: "../../../images/avatar/3.png",
+    postId: 4,
+    music: {
+      url: "http://other.web.nc01.sycdn.kuwo.cn/resource/n2/17/41/650858910.mp3",
+      title: "未知 - stay with me",
+      coverImg: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1551515348&di=0fc6a3b968cebb2f5501ef23b705cb64&src=http://img.cndesign.com/Works/20151217/dae0fa9947b34343b68cdabac47c997c.jpeg"
+    }
+  },
+  {
+    title: "新聞消費",
+    content: "2019年中国10大消费趋势：精专主义，疗愈经济，国货新潮…",
+    imgSrc: "/images/post/06.jpg",
+    reading: 96,
+    detail: "当2019年已经匆匆来临，当很多企业决策者和营销人都在制定年度的增长指标和营销预算的时候，大家都在关心同一个话题：“趋势”。经历了2018年中美贸易战等因素的影响，有的企业认为2019年要激流勇进，有的企业认为要放慢脚步，而中国消费市场似乎也变得更加扑朔迷离。有人在讨论戴威的固执和ofo的危机是不是敲响了互联网独角兽公司的警钟？有人在论证瑞幸咖啡是不是能够真的打破星巴克多年不变的商业模式？有人在质疑小罐茶的大师到底是不是真的在亲手做每一罐茶？还有人期待自己能够打造出一个《啥是佩奇》这样的爆款营销…………人们对于商业世界的各种创新，总是充满着复杂的心情，层出不穷的商业案例涌现的背后，究竟有没有规律可循？",
+    collection: 26,
+    dateTime: "21小时前",
+    headImgSrc: "/images/post/06.jpg",
+    author: "法海",
+    date: "Nov 20 2016",
+    avatar: "../../../images/avatar/5.png",
+    postId: 5,
+    music: {
+      url: "http://other.web.nc01.sycdn.kuwo.cn/resource/n2/4/87/2744094383.mp3",
+      title: "未知 - stay with me",
+      coverImg: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1551515348&di=0fc6a3b968cebb2f5501ef23b705cb64&src=http://img.cndesign.com/Works/20151217/dae0fa9947b34343b68cdabac47c997c.jpeg"
+    }
+  },
+]
+
+module.exports = {
+  postList: local_database
+}
